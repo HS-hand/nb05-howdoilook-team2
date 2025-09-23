91dd2031576157d7ae84e2b9c35e369f93dd083d
HS-hand <shs0714@khu.ac.kr>
2025-09-23 10:41:17 +0900

    Revert "정인성 "

--- src/02-middleware/res.dto/comment/create.comment.res.dto.js
+++
@@ -1,9 +0,0 @@
-export class CreateCommentResDto {
-  constructor(createdComment) {
-    this.id = createdComment.id;
-    this.nickname = createdComment.nickname;
-    this.curationID = createdComment.curationID;
-    this.content = createdComment.content;
-    this.createdAt = createdComment.createdAt;
-  }
-}
--- src/02-middleware/res.dto/style/delete.style.res.dto.js
+++
@@ -1,5 +0,0 @@
-export class DeleteStyleResDto {
-  constructor() {
-    this.message = "스타일 삭제 성공";
-  }
-}
