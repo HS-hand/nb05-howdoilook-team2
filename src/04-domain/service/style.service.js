f283f67b1e8e909e04f887e78df9e4c3004a4468
GhostGN95 <wcxie1234@gmail.com>
2025-09-23 13:27:02 +0900

    update: 04-domain 스타일 부분 업데이트

--- src/04-domain/service/style.service.js
+++ src/04-domain/service/style.service.js
@@ -8,6 +8,16 @@ export class StyleService {
     this.#styleRepo = styleRepo;
   }
 
+  async getStyleList(options = {}) {
+    const result = await this.#styleRepo.findAll(options);
+    return result;
+  }
+
+  async getPopularTags(limit = 10) {
+    const tags = await this.#styleRepo.getPopularTags(limit);
+    return tags;
+  }
+
   async createStyle(styleData) {
     const styleEntity = Style.factory(styleData);
     const createdStyle = await this.#styleRepo.create(styleEntity, styleData);
@@ -36,6 +46,7 @@ export class StyleService {
     if (!passwordMatch) {
       throw new Exception(EXCEPTIONS.FORBIDDEN);
     }
+
     const updatedStyle = await this.#styleRepo.update(styleId, rest);
     return updatedStyle;
   }
