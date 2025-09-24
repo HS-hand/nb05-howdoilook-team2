a68029525f47f9f6af7a7bc867486a94f1375bd4
GhostGN95 <wcxie1234@gmail.com>
2025-09-24 09:56:15 +0900

    update: 지원님 validator 및 dto 추가와 미들웨어, 컨트롤러, 레포지토리 update

--- src/02-middleware/style.middleware.js
+++ src/02-middleware/style.middleware.js
@@ -1,10 +1,12 @@
 import { CreateStyleValidator } from "./req.validator/style/create.style.req.validator.js";
 import { UpdateStyleValidator } from "./req.validator/style/update.style.req.validator.js";
 import { DeleteStyleValidator } from "./req.validator/style/delete.style.req.validator.js";
+import { ListStyleValidator } from "./req.validator/style/list.style.req.validator.js";
 import { CreateStyleResDto } from "./res.dto/style/create.style.res.dto.js";
 import { UpdateStyleResDto } from "./res.dto/style/update.style.res.dto.js";
 import { DeleteStyleResDto } from "./res.dto/style/delete.style.res.dto.js";
-import { StyleDetailResDto } from "./res.dto/style/style.detail.res.dto.js";
+import { GetStyleDetailResDto } from "./res.dto/style/get.style.detail.res.dto.js";
+import { ListStyleResDto } from "./res.dto/style/list.style.res.dto.js";
 
 export class StyleMiddleware {
   #styleService;
@@ -14,44 +16,19 @@ export class StyleMiddleware {
   }
 
   galleryListMiddleware = async (req, res, next) => {
-    const {
-      page = "1",
-      pageSize = "10",
-      sortBy = "latest",
-      tag,
-      searchBy,
-      keyword,
-    } = req.query;
+    const validator = new ListStyleValidator({ query: req.query });
+    const validated = validator.validate();
 
-    const options = {
-      page: Number(page) || 1,
-      pageSize: Number(pageSize) || 10,
-      sortBy,
-      tagFilter: tag,
-      searchBy,
-      keyword,
-    };
-
-    const { items, pagination } =
-      await this.#styleService.getStyleList(options);
-
-    const listDto = items.map((style) => {
-      const representativeImage =
-        style.imageUrls && style.imageUrls.length ? style.imageUrls[0] : null;
-
-      return {
-        id: style.id,
-        representativeImage,
-        title: style.title,
-        nickname: style.nickname,
-        tags: style.tags,
-        categories: style.categories,
-        content: style.content,
-        viewCount: style.viewCount,
-        curationCount: style.curationCount ?? 0,
-        createdAt: style.createdAt,
-      };
+    const { items, pagination } = await this.#styleService.getStyleList({
+      page: validated.page,
+      pageSize: validated.pageSize,
+      sortBy: validated.sortBy,
+      tagFilter: validated.tag,
+      searchBy: validated.searchBy,
+      keyword: validated.keyword,
     });
+
+    const listDto = items.map((style) => new ListStyleResDto(style));
 
     return res.json({
       items: listDto,
@@ -76,10 +53,10 @@ export class StyleMiddleware {
     return res.status(201).json(responseDto);
   };
 
-  viewStyleDetailMiddleware = async (req, res, next) => {
+  getStyleDetailMiddleware = async (req, res, next) => {
     const styleId = req.params.styleId;
     const styleEntity = await this.#styleService.getStyleById(styleId);
-    const responseDto = new StyleDetailResDto(styleEntity);
+    const responseDto = new GetStyleDetailResDto(styleEntity);
 
     return res.status(200).json(responseDto);
   };
