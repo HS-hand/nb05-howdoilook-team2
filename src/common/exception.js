4f1fbecf7692f05fc3c9c4e75dc2638db8fa6dae
jung-insung <yon9170@naver.com>
2025-09-29 10:24:29 +0900

    fix: 비속어 에러 409로 바꿈

--- src/common/exception.js
+++ src/common/exception.js
@@ -167,8 +167,8 @@ export const EXCEPTIONS = {
     message: "비밀번호가 중복되었습니다. 다시 입력하세요.",
   },
   VULGAR_LANGUAGE: {
-    statusCode: 400,
-    message: "비속어가 포함되었습니다. 다시 입력하세요.",
+    statusCode: 409,
+    message: "내용에 비속어가 포함되었습니다. 다시 입력하세요.",
   },
   RANKBY_FORM: {
     statusCode: 400,
